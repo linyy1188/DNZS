e2b35fcda0daed3f086f54b4ce588131917e35cb
linyy1188 <linyy1188@163.com>
2012-12-27 13:15:03 +0800

    2012-12-27 13:14:51

---
+++ sample/js/appointment.js
@@ -0,0 +1,9 @@
+
+$('#dateset').buttonset();
+$('#dateset').tooltip({
+ placement:'bottom'
+ });
+
+$('#num').css("line-height","15px");
+$('#num').css("width","50px");
+$('#num').css("height","15px");
