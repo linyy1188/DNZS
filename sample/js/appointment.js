37d268009bd17188f50533e8ed0d902ff647ef0b
linyy1188 <linyy1188@163.com>
2012-12-27 21:04:57 +0800

    2012-12-27 21:04:51
    Signed-off-by: linyy1188 <linyy1188@163.com>

--- sample/js/appointment.js
+++ sample/js/appointment.js
@@ -1,9 +1,35 @@
-
+//----init
 $('#dateset').buttonset();
 $('#dateset').tooltip({
  placement:'bottom'
  });
 
 $('#num').css("line-height","15px");
-$('#num').css("width","50px");
+$('#num').css("width","80px");
 $('#num').css("height","15px");
+
+
+
+//-----显示已预订人数
+
+$(document).ready(function(){	
+$(':radio').click(function(){
+	//修改提示
+	var i,s;
+		 for (i=1;i<=5;i++)
+	  { 
+	   s="date"+i
+	 if ($(this).attr('id')==s )
+		 $('#num').html("已有"+i+"人预约")
+		 }
+
+	$('#num').css("visibility","visible");
+
+})
+})
+$(document).ready(function() {
+    $(':reset').click(function(){
+			$('#num').css("visibility","hidden");
+		})
+})
+
