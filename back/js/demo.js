d0a5ec1c1a2ad3a424c0062b0897a4a24bf0b62e
cdowen <hhylx1950@gmail.com>
2013-01-02 23:54:19 +0800

    complete function, debug next 	modified:   back/js/demo.js 	back/getdata.php

--- back/js/demo.js
+++ back/js/demo.js
@@ -16,3 +16,41 @@ $(function () {
     });
 
 });
+
+$('#ui-id-1').click(function () {
+  htmlobj=$.ajax({
+    url:"getdata.php?type=consult",
+    success:function(data) {
+      for (var i = data.length - 1; i >= 0; i--) {
+        $('#tabs-1 table tbody').append(
+          '<tr><td>'+data[i].contact+'</td><td>'+data[i].des+
+          '</td><td><button class="btn btn-primary reply">回复</button></td></tr>');
+      };
+    }})
+}
+)
+
+$('#ui-id-2').click(function () {
+  htmlobj=$.ajax({
+    url:"getdata.php?type=appointment",
+    success:function(data) {
+      for (var i = data.length - 1; i >= 0; i--) {
+        $('#tabs-1 table tbody').append(
+          '<tr><td>'+data[i].name+'</td><td>'+data[i].contact+
+          '</td><td>week'+data[i].time+'</td><td>'+data[i].des+'</td><tr>');
+      };
+    }})
+}
+)
+
+$('#ui-id-3').click(function () {
+  htmlobj=$.ajax({
+    url:"getdata.php?type=message",
+    success:function(data) {
+      for (var i = data.length - 1; i >= 0; i--) {
+        $('#tabs-1 table tbody').append(
+          '<tr><td>'+data[i].contact+'</td><td>'+data[i].des+'</td><tr>');
+      };
+    }})
+}
+)
